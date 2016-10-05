9884222cbc3aa37242ba639b2bce98446ec8c4df
Vladimir Larin (vubuntu) <vlarine@gmail.com>
2016-10-05 22:49:39 +0300

    Changed project filename

--- setup.py
+++ setup.py
@@ -7,5 +7,10 @@ setup(
     name='pyrecsys',
     version=pyrecsys.__version__,
     packages=find_packages(),
+    author = "Vladimir Larin",
+    author_email = "vladimir@vlarine.ru",
+    description = "Collaborative filtering recommender system",
+    license = "MIT",
+    url = "https://github.com/vlarine/pyrecsys",
     long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
